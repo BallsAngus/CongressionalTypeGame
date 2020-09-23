716c134574f8ccf75c6a781fcdfc103c5328010d
BallsAngus <71794101+BallsAngus@users.noreply.github.com>
2020-09-23 14:42:26 -0700

    Add files via upload

---
+++ Assets/Scripts/WordInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WordInput : MonoBehaviour
+{
+    public WordManager wordManager;
+    public InputField textInput;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            /*
+            foreach(char letter in textInput.text)
+            {
+                wordManager.TypeLetter(letter);
+            }
+            */
+            wordManager.TypeWord(textInput.text);
+            textInput.ActivateInputField();
+            textInput.Select();
+            textInput.text = "";
+        }
+
+    }
+}
---
+++ Assets/Scripts/WordManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordManager : MonoBehaviour
+{
+    /*
+        TODO:
+
+       
+        - WPM, Score, and Accuracy Code
+        - Create a text file reader for large lists of words.
+        - Make it all look nice :)
+
+
+        DONE:
+
+        - Create an actual working text field.
+        - Change Input Field Mechanism to not accept input until word
+        typed correctly.
+
+    */
+
+
+
+
+    //Space theme, possble names: Otherwordly
+    // Difficulty: Word Sizes and Speed
+    public List<Word> words;
+    public WordSpawner wordSpawner;
+
+    private bool hasActiveWord;
+    private Word activeWord; 
+
+    public void AddWord()
+    {
+        Word word = new Word(WordGenerator.GetRandomWord(), wordSpawner.SpawnWord());
+        Debug.Log(word.word);
+
+        words.Add(word);
+    }
+
+    public void TypeLetter(char letter)
+    {
+        if(hasActiveWord)
+        {
+            // Check if letter was next
+                // Remove it from the word
+            if(activeWord.GetNextLetter() == letter)
+            {
+                activeWord.TypeLetter();
+            }
+        }
+        else
+        {
+            foreach(Word word in words)
+            {
+                if (word.GetNextLetter() == letter)
+                {
+                    activeWord = word;
+                    hasActiveWord = true;
+                    word.TypeLetter();
+                    break;
+                }
+            }
+        }
+
+        if(hasActiveWord && activeWord.WordTyped())
+        {
+            hasActiveWord = false;
+            words.Remove(activeWord);
+        }
+    }
+    
+    public void TypeWord(string inputWord)
+    {
+        foreach(Word word in words)
+        {
+            if (word.GetNextLetter() == inputWord[0])
+            {
+                activeWord = word;
+                if(inputWord == word.word)
+                {
+                    words.Remove(activeWord);
+                    for(int i = 0; i < word.word.Length; i++)
+                    {
+                        word.TypeLetter();
+                    }
+                    break;
+                }
+            }
+        }
+
+    }
+    
+    
+}
